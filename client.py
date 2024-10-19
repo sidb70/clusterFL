495c38dfa3c86f71f1609911ab83599fc4119ad5
SB <bhatta70@msu.edu>
2024-10-19 17:26:26 -0400

    normalize grad updates per batch

--- client.py
+++ client.py
@@ -14,19 +14,45 @@ class Client:
         '''
         # accumulate gradients, do not update weights
         model.train()
+        model.to(self.device)
         for x, y in data_loader:
             x, y = x.to(self.device), y.to(self.device)
             output = model.forward(x)
-            loss = criterion(output, y)
+            loss = criterion(output, y) / len(data_loader)
             loss.backward()
         grad_state_dict = {}
         for name, param in model.named_parameters():
             if param.grad is not None:
                 grad_state_dict[name] = param.grad.clone()
+                param.grad.zero_()
             else:
                 grad_state_dict[name] = torch.zeros_like(param)
         return grad_state_dict
     
+    def train(self, model, data_loader, criterion, optimizer, num_epochs: int):
+        '''
+        Train the model on the given data loader for the given number of epochs.
+
+        Returns:
+            None
+        '''
+        model.train()
+        model.to(self.device)
+        for epoch in range(num_epochs):
+            optimizer.zero_grad()
+            for i, (x, y) in enumerate(data_loader):
+                x, y = x.to(self.device), y.to(self.device)
+                
+                output = model.forward(x)
+                loss = criterion(output, y)/len(data_loader)
+                loss.backward()
+            # optimizer.step()
+            with torch.no_grad():
+                for name, param in model.named_parameters():
+                    if param.grad is not None:
+                        param -= 0.4*param.grad
+                        param.grad.zero_()
+            optimizer.zero_grad()
     def evaluate(self, model, data_loader, criterion) -> Tuple[float, float]:
         '''
         Evaluate the model on the given data loader.
@@ -35,6 +61,7 @@ class Client:
             Tuple[float, float]: A tuple containing the average loss and accuracy of the model.
         '''
         model.eval()
+        model.to(self.device)
         total_loss = 0
         total_correct = 0
         total_samples = 0
--- tests/test_model_train.py
+++ tests/test_model_train.py
@@ -8,6 +8,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+
+torch.manual_seed(0)
 class TestModelTrain(unittest.TestCase):
     def test_train(self):
         trainset, testset = load_cifar10()
@@ -16,9 +18,11 @@ class TestModelTrain(unittest.TestCase):
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         client = Client(id=0, device=torch.device(device))
         model = load_model('cnn')
+        model.requires_grad_(True)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-        for i in range(10):
+        optimizer = optim.SGD(model.parameters(), lr=0.05, momentum=0.9)
+        # optimizer = optim.Adam(model.parameters(), lr=0.01)
+        for i in range(100):
             model.train()
             grad_dict = client.compute_gradients(model, train_loader, criterion)
             optimizer.zero_grad()
@@ -26,17 +30,23 @@ class TestModelTrain(unittest.TestCase):
                 self.assertEqual(grad_dict[name].shape, param.shape)
                 # set model gradients to the computed gradients
                 param.grad = grad_dict[name]
-
-            optimizer.step()
+            with torch.no_grad():
+                for name, param in model.named_parameters():
+                    grad = grad_dict[name]
+                    param -= 0.4 * grad
+                    param.grad.zero_()
             optimizer.zero_grad()
             for name, param in model.named_parameters():
                 self.assertIsNone(param.grad)
 
+            # client.train(model, train_loader, criterion, optimizer, num_epochs=1)
 
             model.eval()
             with torch.no_grad():
                 loss, acc = client.evaluate(model, test_loader, criterion)
                 print(f'Epoch {i+1}, Loss: {loss:.4f}, Accuracy: {acc:.4f}')
+
+        self.assertGreater(acc, 0.3)
 
 
 
