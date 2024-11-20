5940fd59ebfdb10b99ad6651cd320a1d49d88942
Siddhartha Bhattacharya <bhatta70@msu.edu>
2024-11-20 09:25:27 -0500

    implement max_matching_dist

--- cluster.py
+++ cluster.py
@@ -3,6 +3,9 @@ import random
 from typing import Dict, List
 import torch
 from aggregation.strategies import load_aggregator
+import numpy as np
+from sklearn.cluster import KMeans
+
 
 torch.manual_seed(0)
 random.seed(0) 
@@ -57,6 +60,41 @@ class ClusterDaddy:
             normalizedWeights.append(normalWeight)
 
         return normalizedWeights
+    
+    def filter_dist(self, filter1, filter2):
+        return torch.dist( filter1, filter2,p=1)
+    def compute_filter_dist(self, client1_filters, client2_filters):
+        dist_mat = np.zeros((len(client1_filters), len(client2_filters)))
+        for i, filter1 in enumerate(client1_filters):
+            for j, filter2 in enumerate(client2_filters):
+                dist_mat[i, j] = self.filter_dist(filter1, filter2)
+        return dist_mat
+    def match_filters(self,dist_mat):
+        # match filters. once two filters are matched, remove them from the list
+        matches = []
+        for _ in range(dist_mat.shape[0]):
+            i, j = np.unravel_index(np.argmin(dist_mat, axis=None), dist_mat.shape)
+            dist = dist_mat[i, j]
+            matches.append((i, j, dist))
+            dist_mat[i, :] = np.inf
+            dist_mat[:, j] = np.inf
+        return matches
+    def max_matching_dist(self, layer_weights):
+        '''
+        Compute the maximum matching distance between all the clients for one layer
+        Args:
+            layer_weights: list of weights for one layer for all the clients
+        Returns:
+            dist_mat: matrix of distances between all the clients
+        '''
+        dist_mat = np.zeros((len(layer_weights), len(layer_weights)))
+        for i, client1_weights in enumerate(layer_weights):
+            for j, client2_weights in enumerate(layer_weights):
+                filter_dists = self.compute_filter_dist(client1_weights, client2_weights)
+                matches = self.match_filters(filter_dists)
+                max_dist = max([match[2] for match in matches])
+                dist_mat[i, j] = max_dist
+        return dist_mat
     def _kmeans_dist_map(self, weights, clusterList):
         distanceMap = {}
         for client, weight in enumerate(weights):
@@ -70,6 +108,16 @@ class ClusterDaddy:
                 distanceMap[client].append(dist)
         return distanceMap
     def kMeans(self, k_iter: int = 10, normalize: bool = False):
+        km = KMeans(n_clusters=self.clusters)
+        # take only first layer
+        weights = [list(weight.values())[0] for weight in self.weights]
+        dist_mat = self.max_matching_dist(weights)
+        km.fit(dist_mat)
+        clusterList = [[] for i in range(self.clusters)]
+        for i, label in enumerate(km.labels_):
+            clusterList[label].append(i)
+        return clusterList
+
         # Assume we have a fixed number clusters
         clusterList = [[] for i in range(self.clusters)]
         clients = [i for i in range(len(self.weights))]
