c8da65cec848e88f3ec75f19795a6d7e1f4bcc90
Siddhartha Bhattacharya <bhatta70@msu.edu>
2024-12-03 19:23:31 -0500

    create bigger cifar100cnn

--- models/cnn.py
+++ models/cnn.py
@@ -49,28 +49,44 @@ class Cifar100CNN(nn.Module):
     A simple CNN for classifying CIFAR-100 images.
     """
 
-    def __init__(self, out_dim=10):
+    def __init__(self):
         super(Cifar100CNN, self).__init__()
         self.nn =nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
+            # First block
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Second block
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Third block
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            
+            # Fully connected layer
             nn.Flatten(),
-            nn.Linear(128 * 2 * 2, 256),
-            nn.Linear(256, 100)
+            nn.Linear(128 * 4 * 4, 512),
+            nn.ReLU(),
+            nn.Linear(512, 100)
         )
+        self.apply(init_weights)
+
     def forward(self, x):
         return self.nn(x)
 
